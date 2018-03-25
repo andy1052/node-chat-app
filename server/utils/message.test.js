91ce4a37db46dec308ed75d1118b29f1c56dac55
Andre <andreducharme@andre-ducharmes-macbook.local>
2018-03-24 21:48:12 -0600

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,23 @@
+const expect = require('expect');
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+	it ('Should generate the correct message object', () => {
+		var from = 'Jen';
+		var text = 'some message';
+
+		var message = generateMessage(from, text);
+		console.log(from);
+
+
+		expect(message.createdAt).toBeA('number');
+		expect(message).toInclude({
+			from,
+			text
+		});
+		//store res in variable
+		//assert from is correct
+		//assert text match
+		//assert createdAt value is a number toBeA('number');
+	});
+});
